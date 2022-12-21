0307f51c42e2640238f8efd1d259f8d0e189bd95
Winston Chan <95652335+winstuhn@users.noreply.github.com>
2022-12-21 13:13:54 -0800

    Delete src directory

--- src/data/exercises.js
+++
@@ -1,6 +0,0 @@
-const exercises_list = [
-    {name:"Curls", reps:8, weight:125, unit:"lbs", date:"Monday"},
-    {name:"Curls", reps:8, weight:125, unit:"lbs", date:"Monday"}
-];
-
-export default exercises_list;
